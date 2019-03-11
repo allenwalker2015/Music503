616335dfd72e6fc34101d8d12da7604991ab5146
Emilio Forrer <dev.emilio.forrer@gmail.com>
2019-03-10 21:55:01 -0600

    ✨ configuration for production

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-puma: bundle exec puma -C config/puma.rb
-webpack: ./docker/webpacker.sh
-nginx: ./docker/nginx.sh
+/workspace/docker/Procfile
--- docker/startup.sh
+++ docker/startup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-cp $APP_DIR/docker/Procfile .
+
 mkdir -p tmp/sockets/
 
 if [[ $STAGE == "production" || $STAGE == "staging" ]]; then
+  ln -sf  $APP_DIR/docker/Procfile ./Procfile
   bundle exec rails db:create
   bundle install --jobs=10 --without development test
+  bundle exec rails assets:precompile
+  bundle exec rails webpacker:compile
 else
+  ln -sf  $APP_DIR/docker/Procfile.dev ./Procfile
   bundle install --jobs=10
 fi
 
