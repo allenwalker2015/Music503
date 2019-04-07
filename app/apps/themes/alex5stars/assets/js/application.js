65d77ab09a4c57a2c11789cb13e92c87c4a644eb
Emilio Forrer <dev.emilio.forrer@gmail.com>
2019-04-07 14:37:33 -0600

    Added sliders and social links

--- app/apps/themes/alex5stars/assets/js/application.js
+++ app/apps/themes/alex5stars/assets/js/application.js
@@ -1,2 +1,26 @@
 //= require jquery3
 //= require social-share-button
+//= require ./slick.min.js
+
+$(document).ready(function () {
+  var sliderOptions = {
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
+  $('.slick-noticias').slick(sliderOptions);
+  $('.slick-musica').slick(sliderOptions);
+  $('.slick-videos').slick(sliderOptions);
+})
